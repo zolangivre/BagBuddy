285a1f0653d35a97947159add3e4cff3dc0af63b
Zolan Givre <zolannp2602@gmail.com>
2025-10-26 17:15:29 +0100

    ajout de constantes pour gerer les status, les aeroport etc

---
+++ constants/allConstants.js
@@ -0,0 +1,19 @@
+export const USER_ROLE = {
+  SELLER: "seller",
+  BUYER: "buyer",
+};
+
+export const USER_ROLE_LABELS = {
+  [USER_ROLE.SELLER]: "Seller",
+  [USER_ROLE.BUYER]: "Buyer",
+};
+
+export const USER_TRANSACTION_TYPE = {
+  BUYING: "buying",
+  SELLING: "selling",
+};
+
+export const USER_TRANSACTION_TYPE_LABELS = {
+  [USER_TRANSACTION_TYPE.BUYING]: "Buying",
+  [USER_TRANSACTION_TYPE.SELLING]: "Selling",
+};
