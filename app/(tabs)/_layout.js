d95b01ae1adc8540167a98d7d40dee7d1d931da0
Zolan Givre <zolannp2602@gmail.com>
2025-10-26 17:18:22 +0100

    changement de navigation bar

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,22 +1,37 @@
-import { Stack } from "expo-router";
-import BottomTabNavigator from "../../components/BottomTabNavigator";
-import { View, StyleSheet } from "react-native";
+import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
+import { useLanguage } from "@/contexts/LanguageContext";
+import { useEffect, useState } from "react";
 
 export default function TabsLayout() {
+  const { i18n, language } = useLanguage();
+  const [tabLabels, setTabLabels] = useState({
+    home: i18n.t("home"),
+    transactions: i18n.t("transactions"),
+    profile: i18n.t("profile"),
+  });
+
+  useEffect(() => {
+    setTabLabels({
+      home: i18n.t("home"),
+      transactions: i18n.t("transactions"),
+      profile: i18n.t("profile"),
+    });
+  }, [language, i18n]);
+
   return (
-    <View style={styles.container}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="home" />
-        <Stack.Screen name="transactions" />
-        <Stack.Screen name="profile" />
-      </Stack>
-      <BottomTabNavigator />
-    </View>
+    <NativeTabs key={language}>
+      <NativeTabs.Trigger name="home">
+        <Label>{tabLabels.home}</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="transactions">
+        <Icon sf="creditcard" drawable="custom_creditcard_drawable" />
+        <Label>{tabLabels.transactions}</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <Icon sf="person" drawable="custom_person_drawable" />
+        <Label>{tabLabels.profile}</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
