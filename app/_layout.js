c5b4d79f8bc814388632dbcedc9ddd7b4de0ebae
Zolan Givre <zolannp2602@gmail.com>
2025-10-26 17:13:32 +0100

    ajout du choix de langage et du mode dark

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,19 @@
 import { Stack } from "expo-router";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="transaction-detail" />
-    </Stack>
+    <LanguageProvider>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="edit-profile" />
+          <Stack.Screen name="edit-listing" />
+          <Stack.Screen name="transaction-detail" />
+        </Stack>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
