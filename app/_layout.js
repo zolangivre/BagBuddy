91b861f5a0a7b260a31c1a68cb86ac1ed0fbdbd5
Zolan Givre <zolannp2602@gmail.com>
2025-11-03 12:53:00 +0100

    ajout apollo client pour les requetes graphql

--- app/_layout.js
+++ app/_layout.js
@@ -1,19 +1,24 @@
 import { Stack } from "expo-router";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import client from "@/lib/apolloClient";
+import { ApolloProvider } from "@apollo/client/react";
 
 export default function RootLayout() {
   return (
-    <LanguageProvider>
-      <ThemeProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="edit-profile" />
-          <Stack.Screen name="edit-listing" />
-          <Stack.Screen name="transaction-detail" />
-        </Stack>
-      </ThemeProvider>
-    </LanguageProvider>
+      <ApolloProvider client={client}>
+        <LanguageProvider>
+          <ThemeProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="edit-profile" />
+              <Stack.Screen name="edit-listing" />
+              <Stack.Screen name="transaction-detail" />
+              <Stack.Screen name="profile-view" />
+            </Stack>
+          </ThemeProvider>
+        </LanguageProvider>
+      </ApolloProvider>
   );
 }
