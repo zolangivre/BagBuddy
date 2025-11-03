7b8bbc131a6e7192e8d4b7caa8d4823f6d413e03
Zolan Givre <zolannp2602@gmail.com>
2025-11-03 14:52:08 +0100

    ajout dun style global pour les card

---
+++ theme/Styles.js
@@ -0,0 +1,12 @@
+export const globalStyles = {
+  card: {
+    borderRadius: 16,
+    padding: 24,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 3,
+    width: "100%",
+  },
+};
