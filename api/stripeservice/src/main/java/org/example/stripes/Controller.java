62fe8a15891f8874da5c4c08186ce4d08e0b2b93
mathis <mathis.friess@gmail.com>
2025-11-18 15:58:46 +0100

    ajout @request mapping stripe

--- api/stripeservice/src/main/java/org/example/stripes/Controller.java
+++ api/stripeservice/src/main/java/org/example/stripes/Controller.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.example.stripes.StripeService;
 
 @RestController
+@RequestMapping("/stripe")
 public class Controller {
 
     private final StripeService stripeService;
