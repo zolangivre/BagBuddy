da773241272de9ab2fc6bb162a5833e25c14a0ea
Zolan Givre <zolannp2602@gmail.com>
2025-11-21 17:27:28 +0100

    fix: probleme de push

---
+++ stripeservice/src/test/java/com/gateway/stripeservice/StripeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.stripeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
