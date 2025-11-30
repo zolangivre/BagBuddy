de9d2e6930f4df78944db3d98d361caab6987385
Zolan Givre <zolannp2602@gmail.com>
2025-11-30 16:06:33 +0100

    refactor: suppression de fichier sql inutiles

--- reviewservice/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO review (
-  reviewer_id, reviewee_id, trip_id, transaction_id,
-  rating, comment, created_at
-) VALUES
-(10, 20, 1, 1, 5, 'Super échange, très ponctuel.', NOW()),
-(11, 20, null, 2, 4, 'Bon contact, tout s''est bien passé.', NOW());
