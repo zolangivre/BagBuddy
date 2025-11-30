2fe138c7471d672f573da7d922323542c5d701e1
Zolan Givre <zolannp2602@gmail.com>
2025-11-30 20:13:14 +0100

    fix: oublie dun import

--- stripeservice/src/main/java/com/bagbuddy/stripeservice/controller/StripeController.java
+++ stripeservice/src/main/java/com/bagbuddy/stripeservice/controller/StripeController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
 import com.stripe.model.PaymentIntent;
-
+import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 
